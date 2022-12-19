1333127ebef11548049b5fd6833cfb6a6ef3316e
The Shadow <74643315+amarochadli92@users.noreply.github.com>
2022-12-19 18:27:10 +0100

    New Work

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-// Get Element
-const text_logo = document.querySelector(".logo h1");
+const nav_bar = document.querySelectorAll(".link");
+
+// Set Options
+slidClasses(nav_bar, "active");
+
+function slidClasses(target, str) {
+    target.forEach((e) => {
+        e.onclick = () => {
+            removeAllClasses(target, str);
+            e.classList.add("active");
+        };
+    });
+}
+
+function removeAllClasses(target, str) {
+    target.forEach((e) => {
+        e.classList.remove(str);
+    });
+}
---
+++ src/sliders.js
@@ -0,0 +1,29 @@
+import { tns } from "../node_modules/tiny-slider/src/tiny-slider.js";
+var slider = tns({
+    container: "#rewind",
+    items: 3,
+    rewind: true,
+    swipeAngle: false,
+    speed: 400,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        768: {
+            items: 1,
+        },
+        768: {
+            items: 2,
+        },
+        992: {
+            items: 2,
+        },
+        992: {
+            items: 3,
+        },
+    },
+});
+let btn = document.querySelectorAll(".band .tns-outer .tns-controls button");
+btn[0].innerHTML = '<i class="fa-solid fa-circle-left"></i>';
+btn[1].innerHTML = '<i class="fa-solid fa-circle-right"></i>';
